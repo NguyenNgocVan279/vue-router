f37f24ea3790c6371716598fb638b8a77032ebdb
NguyenNgocVan279 <nguyenngocvan279@gmail.com>
2021-05-17 21:37:54 +0700

    L14.Create a 404 Not Found route and page with Vue Router

---
+++ src/views/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>Không tìm thấy</h1>
+        <p>
+            Ops chúng tôi không tìm thấy trang này. Thử đi tới
+            <router-link :to="{ name: 'Home' }">Trang chủ</router-link>
+        </p>
+    </div>
+</template>
